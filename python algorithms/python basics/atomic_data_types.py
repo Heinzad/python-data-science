c6a0718aa6ba919cd40a0333b989e258e75bca6d
Ad Heinz <Heinzad@users.noreply.github.com>
2023-08-12 10:30:12 +1200

    Python Atomic Data Types Revision, dated 2022-08-12

---
+++ python algorithms/python basics/atomic_data_types.py
@@ -0,0 +1,53 @@
+""" atomic_data_types.py 
+Exercises from Chaper 1 of Miller & Ranum "Problem Solving With Algorithms and Data Structures Using Python" 
+""" 
+
+import doctest 
+
+def get_result(answer, expected, label='_'): 
+    """ displays if answer matches what was expected 
+    
+    # doctests 
+    >>> test = get_result(1+1,2,'(a)')
+    question (a) expected: 2, got: 2, result: Pass
+    >>> test
+    'Pass'
+
+    """ 
+    result = 'Fail'
+    if expected == answer: 
+        result = 'Pass' 
+    print(f"question {label} expected: {expected}, got: {answer}, result: {result}")
+    return result 
+
+
+def run_tests():  
+    """run through docstring tests """ 
+    doctest.run_docstring_examples(get_result, None) 
+     
+if __name__ == "__main__": 
+    run_tests() 
+    
+
+# numeric data types 
+
+## integer calculations 
+
+get_result(2+3*4, 14, 1) 
+get_result((2+3)*4, 20, 2) 
+get_result(2**10, 1024, 3) 
+
+## floating point calculations  
+get_result(6/3, 2.0, 4)
+get_result(7/3, 2.3333333333333335, 5) 
+get_result(7//3, 2,6) # floor 
+get_result(7%3, 1, 7) # modulo 
+get_result(3/6, 0.5, 8) 
+get_result(3//6, 0, 9) # __floordiv__() 
+get_result(3%6, 3, 10) 
+get_result(2**100, 1267650600228229401496703205376, 11) # exponentiation 
+
+
+
+
+
