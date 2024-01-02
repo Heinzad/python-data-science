7475b1f228a708b49cfa819f284bb37f04fb1b18
Ad Heinz <Heinzad@users.noreply.github.com>
2024-01-02 16:58:41 +1300

    Insertion Sort -- added a count of the number of comparisons made

--- python algorithms/searching and sorting/insertion_sort.py
+++ python algorithms/searching and sorting/insertion_sort.py
@@ -6,8 +6,9 @@
     Algorithm: 
     A given list is sorted in-place. It is conceptually divided into two sublists: sorted and unsorted. 
     The boundary between the two sublists is at index position (i). 
-    -- Everything to the left of i (i.e. sort_list[0]) is the 'sorted sublist' 
-    -- Everything to the right of i (i.e. sort_list[1:]) is the 'unsorted sublist' 
+    The two sublists are described as invariants, defined as: 
+    -- Everything to the left of i (i.e. sort_list[0]) is 'sorted' 
+    -- Everything to the right of i (i.e. sort_list[1:]) is 'unsorted'  
     An outer loop ascends through the unsorted sublist from the left bound to the right bound, 
     launching an innner loop for each unsorted index postition (i):  
     -- The value at the unsorted index position (i) is placed into a temp variable (val), and 
@@ -20,40 +21,63 @@
 import doctest 
 
 def insertion_sort(sort_list): 
-    """Sorts a given list in place 
+    """Sorts a given list in place, returning the number of comparisons 
+    The number of comparisons varies by the size of the list and the extent of pre-sorting: 
+    -- (n-1) for the O(n) best case (a list already sorted in ascending order)
+    -- (n**2 -n)/2 for the O(n**2) worst case (a list already sorted in descending order)
     
-    >>> test_list = [52, 25, 93, 39, 78, 87, 45, 51, 21, 12]
-    >>> insertion_sort(test_list)
-    >>> print(test_list)
-    [12, 21, 25, 39, 45, 51, 52, 78, 87, 93]
-
+    >>> ascending_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> best_case = insertion_sort(ascending_list)
+    >>> print(ascending_list)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> print(best_case)
+    9
+    >>> descending_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    >>> worst_case = insertion_sort(descending_list)
+    >>> print(descending_list)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> print(worst_case)
+    45
+    >>> random_list = [5, 2, 9, 3, 7, 8, 4, 6, 0, 1]
+    >>> average_case = insertion_sort(random_list)
+    >>> print(random_list)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
     """ 
+
+    comparisons = 0 
     
-    # Unsorted Loop: 
-    # ascend through unsorted sublist 
-    for unsorted_idx in range(1, len(sort_list)):  # begin one position higher than sorted sublist 
+    # Outer Loop: ascend through unsorted sublist 
+    for unsorted_key in range(1, len(sort_list)):  # begin one position higher than sorted sublist 
         
-        val = sort_list[unsorted_idx] # next value in unsorted list
-        sorted_idx = unsorted_idx   # set the right bound of the sorted list at the left bound of the unsorted list 
+        unsorted_val = sort_list[unsorted_key] # next value in unsorted list     
+        sorted_key = unsorted_key   # set the right bound of the sorted list at the left bound of the unsorted list 
+        
+        # count implicit comparison inherent in loop critera below
+        if sort_list[sorted_key -1] <= unsorted_val:
+            comparisons += 1
 
-        # Sorted Loop: 
-        # descend through sorted sublist until no greater value lies to the left 
-        while sorted_idx > 0 and sort_list[sorted_idx -1] > val: 
+        else:                
+            # Inner Loop: descend through sorted sublist until no greater value lies to the left 
+            while sorted_key > 0 and sort_list[sorted_key -1] > unsorted_val: 
+                comparisons += 1 
+                
+                # shift higher values up one place to fill the vacated position 
+                sort_list[sorted_key] = sort_list[sorted_key -1] 
 
-            # shift higher values up one place to fill the vacated position 
-            sort_list[sorted_idx] = sort_list[sorted_idx -1] 
-            
-            # descend to the next place in the sorted sublist 
-            sorted_idx = sorted_idx -1  
-
+                # descend to the next place in the sorted sublist 
+                sorted_key = sorted_key -1
+                
         # Write the value into the correct position in the sorted sublist 
-        sort_list[sorted_idx] = val
-
+        sort_list[sorted_key] = unsorted_val 
+        
+    return comparisons 
 
 
 
 if __name__ == "__main__":
     
     print("docstring test start ...")
-    doctest.run_docstring_examples(insertion_sort, None, verbose=False)
+    
+    doctest.run_docstring_examples(insertion_sort, None, verbose=True)
     print("...docstring test end")
